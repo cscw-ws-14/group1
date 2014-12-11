5525ee4a6bf9cfd2f4d7bc3ad5953e6fb786d61c
shekoufeh <shekoufeh.gorgizadeh@gmail.com>
2014-12-11 18:51:46 +0100

    IndoorAirQuality

---
+++ src/main/java/IndoorAirQuality/Duplicator.java
@@ -0,0 +1,54 @@
+/*
+ * By:
+ * Shekoufeh Gorgi Zadeh
+ * 
+ * 10.12.2014
+ * 
+ */
+package IndoorAirQuality;
+
+import com.jpmorrsn.fbp.engine.Component;
+import com.jpmorrsn.fbp.engine.ComponentDescription;
+import com.jpmorrsn.fbp.engine.InPort;
+import com.jpmorrsn.fbp.engine.InputPort;
+import com.jpmorrsn.fbp.engine.OutPort;
+import com.jpmorrsn.fbp.engine.OutputPort;
+import com.jpmorrsn.fbp.engine.Packet;
+
+@ComponentDescription("Duplicates the input on each outport.")
+@OutPort(value = "OUT", arrayPort = true)
+@InPort("IN")
+public class Duplicator extends Component {
+
+  static final String copyright = " ";
+
+  private InputPort inport;
+
+  private OutputPort[] outportArray;
+
+  @Override
+  protected void execute() {
+
+    int no = outportArray.length;
+    Packet p;
+
+    p = inport.receive();
+
+    Packet out;
+    System.out.println(Integer.parseInt((String)p.getContent()));
+    for(int i = 0; i < no; ++i){
+    	out = create(p.getContent());
+    	outportArray[i].send(out);
+    }
+    drop(p);
+  }
+
+  @Override
+  protected void openPorts() {
+
+    inport = openInput("IN");
+
+    outportArray = openOutputArray("OUT");
+
+  }
+}
---
+++ src/main/java/IndoorAirQuality/Mosquitto.java
@@ -0,0 +1,127 @@
+package IndoorAirQuality;
+
+import java.util.StringTokenizer;
+
+import com.jpmorrsn.fbp.engine.*;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/**
+ * By: 
+ * 	Ridho
+ * Edited By:
+ * 	Shekoufeh
+ * 
+ * go to
+ * https://github.com/r4ccoon/javaFBPMosquittoComponent
+ * to get the PAHO mosquittos jar files (java libs that are needed)
+ *
+ * to install mosquitto, on osx-> brew install mosquitto
+ * on windows and linux , http://mosquitto.org/download/ and follow the instructions
+ */
+
+@ComponentDescription("publish a message to the mosquitto server")
+
+@InPorts({
+    @InPort(value = "CLIENTID", description = "client id of the client, can put random string here", type = String.class, optional = true),
+    @InPort(value = "TOPICMESSAGE", description = "topic and message", type = String.class)
+})
+
+public class Mosquitto extends Component{
+
+    private InputPort clientId;
+
+    private InputPort topicMessage;
+
+//    private OutputPort outport;
+
+    // some default values, will be overridden later
+    private String _topic        = "heartbeat";
+    private String _content      = "beat";
+    private String _clientId     = "client_id_";
+
+    // some values that is not necessary to change
+    private int qos             = 2;
+    private String broker       = "tcp://localhost:1883";
+
+    private String toppicmessageStr;
+    @Override
+    protected void execute() throws Exception {
+
+        //
+        _clientId = (String) fillInput(clientId, _clientId);
+        
+    
+        	Packet pT = topicMessage.receive();
+        	
+        	toppicmessageStr    = (String) pT.getContent();
+            drop(pT);
+
+            StringTokenizer st = new StringTokenizer(toppicmessageStr,"^");
+           
+            _topic = st.nextToken();
+            _content = st.nextToken();
+            
+            try {
+                MemoryPersistence persistence = new MemoryPersistence();
+
+                // connect to mqtt server, make sure that the server is on
+                System.out.println("Connecting to broker: " + broker);
+                MqttClient sampleClient = new MqttClient(broker, _clientId, persistence);
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+                sampleClient.connect(connOpts);
+
+                // set up topic and publishing the message
+                System.out.println("Connected");
+                System.out.println(_topic + " is Publishing message: " + _content);
+                MqttMessage message = new MqttMessage(_content.getBytes());
+                message.setQos(qos);
+                sampleClient.publish(_topic, message);
+
+                // disconnect from the server
+                sampleClient.disconnect();
+                //System.out.println("Disconnected");
+
+            } catch(MqttException me) {
+                System.out.println("reason "+me.getReasonCode());
+                System.out.println("msg "+me.getMessage());
+                System.out.println("loc "+me.getLocalizedMessage());
+                System.out.println("cause "+me.getCause());
+                System.out.println("excerpt "+me);
+                me.printStackTrace();
+            }
+        
+    }
+
+    @Override
+    protected void openPorts() {
+        clientId = openInput("CLIENTID");
+        topicMessage =  openInput("TOPICMESSAGE");
+    }
+
+    public Packet checkInput(InputPort inPort) throws Exception {
+        Packet rp = inPort.receive();
+        if (rp == null) {
+            return null;
+        }
+
+        return rp;
+    }
+
+    public Object fillInput(InputPort inPort, Object defaultValue){
+        Packet rp = inPort.receive();
+        if (rp == null) {
+            return defaultValue;
+        }
+
+        Object ret = rp.getContent();
+        drop(rp);
+
+        return ret;
+    }
+}
