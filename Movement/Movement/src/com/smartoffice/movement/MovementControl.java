28fb00bddb002e1a5c4c74ca3eb45f425b735924
Ashish <ashish.mudaliar@yahoo.com>
2015-01-13 20:50:09 +0100

    Started Java FBP Project for movement

---
+++ Movement/Movement/src/com/smartoffice/movement/MovementControl.java
@@ -0,0 +1,13 @@
+package com.smartoffice.movement;
+
+import com.jpmorrsn.fbp.engine.Network;
+
+public class MovementControl extends Network{
+
+	@Override
+	protected void define() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
